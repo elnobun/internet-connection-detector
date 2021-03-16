63d60d7d354f734b75f2eec3f720e680e6979b3d
elnobun <elnobun@gmail.com>
2021-03-16 22:46:43 +0000

    toast notification animation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 /* Select elements */
-const wrapper = document.querySelector(".wrapper")
-const toast = document.querySelector(".toast")
-const wifiIcon = document.querySelector(".icon")
-const title = document.querySelector("span")
-const subTitle = document.querySelector("p")
+const wrapper = document.querySelector('.wrapper')
+const toast = document.querySelector('.toast')
+const wifiIcon = document.querySelector('.icon')
+const title = document.querySelector('span')
+const subTitle = document.querySelector('p')
+const closeIcon = document.querySelector('.close-icon')
 
 /*
 IMPLEMENTING INTERNET CONNECTION DETECTION
@@ -16,53 +17,83 @@ IMPLEMENTING INTERNET CONNECTION DETECTION
 // When windows Load
 window.onload = () => {
     const offline = () => {
-        toast.classList.add("offline")
-        title.innerText = "You are now offline";
-        subTitle.innerText = "Oops! Internet is disconnected";
+        wrapper.classList.remove('hide')
+        toast.classList.add('offline')
+        title.innerText = 'You are now offline'
+        subTitle.innerText = 'Oops! Internet is disconnected'
         wifiIcon.innerHTML = `<i class="uil uil-wifi-slash"></i>`
     }
 
-    const ajax = () => {
-        let xhr = new XMLHttpRequest()
-        xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true)
-        xhr.onload = () => {
-            if (xhr.status >= 200 && xhr.status < 304) {
-                toast.classList.remove("offline")
-                title.innerText = "You are now online";
-                subTitle.innerText = "Nice! You are back online";
-                wifiIcon.innerHTML = `<i class="uil uil-wifi"></i>`
+    // const ajax = () => {
+    //     let xhr = new XMLHttpRequest()
+    //     xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true)
+    //     xhr.onload = () => {
+    //         if (xhr.status === 200 && xhr.status < 300) {
+    //             toast.classList.remove("offline")
+    //             title.innerText = "You are now online";
+    //             subTitle.innerText = "Nice! You are back online";
+    //             wifiIcon.innerHTML = `<i class="uil uil-wifi"></i>`
+    //
+    //
+    //             closeIcon.onclick = () => {
+    //                 wrapper.classList.add("hide")
+    //             }
+    //
+    //             // Hide toast notification after 5 secs
+    //             setTimeout(() => {
+    //                 wrapper.classList.add("hide")
+    //             }, 5000)
+    //
+    //         } else {
+    //             offline()
+    //         }
+    //     }
+    //     xhr.onerror = () => {
+    //         offline()
+    //     }
+    //     xhr.send()
+    // }
+    //
+    // // Refresh windows automatically
+    // setInterval(() => {
+    //     ajax()
+    // }, 100)
+
+    //Declare a checkData function making asynchronous requests
+    const checkData = async () => {
+        // Send a request to a URL
+        try {
+            const data = await fetch(
+                'https://jsonplaceholder.typicode.com/posts')
+            // If the response is 200, user is connected online
+            if (data) {
+                if (data.status === 200 && data.status < 300) {
+                    toast.classList.remove('offline')
+                    title.innerText = 'You are now online'
+                    subTitle.innerText = 'Nice! You are back online'
+                    wifiIcon.innerHTML = `<i class="uil uil-wifi"></i>`
+
+                    closeIcon.onclick = () => {
+                        wrapper.classList.add('hide')
+                    }
+
+                    // Hide toast notification after 5 secs
+                    setTimeout(() => {
+                        wrapper.classList.add('hide')
+                    }, 5000)
+                }
             } else {
                 offline()
             }
-        }
-        xhr.onerror = () => {
+
+        } catch (e) {
+            // Call the offline function
             offline()
         }
-        xhr.send()
     }
 
+    //Call the function
     setInterval(() => {
-        ajax()
+        checkData()
     }, 100)
-
-    // Declare a checkData function making asynchronous requests
-    // const checkData = async () => {
-    //     // Send a request to a URL
-    //     try {
-    //         const data = await fetch("https://jsonplaceholder.typicode.com/posts")
-    //         // If the response is 200, user is connected online
-    //         if (data.status === 200 && data.status < 300) {
-    //             console.log("Online")
-    //         } else {
-    //             // User is not online
-    //             offline()
-    //         }
-    //     } catch (e) {
-    //         // Call the offline function
-    //         offline()
-    //     }
-    // }
-
-    // Call the function
-    //checkData()
 }
